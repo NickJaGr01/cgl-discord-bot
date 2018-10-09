a0e85deb190d2857b706a8b1c45f897b107b5090
Nick Greene <nickjg01@gmail.com>
2018-10-09 13:31:48 -0500

    removed second initialization

--- cgl.py
+++ cgl.py
@@ -2,8 +2,8 @@ import bot
 import os
 from discord.ext import commands
 
-bot.bot = commands.Bot(command_prefix='!')
-bot.MM_CHANNEL_ID = os.environ['MM_CHANNEL_ID']
+#bot.bot = commands.Bot(command_prefix='!')
+#bot.MM_CHANNEL_ID = os.environ['MM_CHANNEL_ID']
 
 import matchmaking
 import cgl_commands
