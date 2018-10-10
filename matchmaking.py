3c7cb68d7bf063a9c0d257c0e8c501a30587ee3f
Nick Greene <nickjg01@gmail.com>
2018-10-10 14:27:20 -0500

    fixed type check

--- matchmaking.py
+++ matchmaking.py
@@ -129,7 +129,7 @@ ELO_K_FACTOR = 16
 
 async def cycle_matches():
     for m in matches:
-        if matches[m]["map"] is list:
+        if type(matches[m]["map"]) is list:
             if matches[m]["time"] % 5 == 0:
                 await matches[m]["channels"][0].send("%s seconds remaining" % matches[m]["time"])
             matches[m]["time"] -= 1
