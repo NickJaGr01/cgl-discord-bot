48913d66c20a6f6df96b9cee99d37614d6d1d711
Nick Greene <nickjg01@gmail.com>
2018-10-15 10:30:00 -0500

    added owner command

--- owner_commands.py
+++ owner_commands.py
@@ -13,6 +13,17 @@ async def giveelo(ctx, target: discord.User, delo: int):
         elo += delo
         database.cur.execute("UPDATE playerTable SET elo=%s WHERE discordID=%s;" % (elo, target.id))
         database.conn.commit()
-        await ctx.send("%s has been given %s elo points." % (target.mention, delo))
+        await ctx.send("%s has been given %s elo." % (target.mention, delo))
+    else:
+        await ctx.send(NOT_OWNER_MESSAGE)
+
+@bot.command()
+async def giverep(ctx, target: discord.User, drep: int):
+    if ctx.message.author.id == bot.appinfo.owner.id:
+        rep = database.player_rep(target.id)
+        rep += drep
+        database.cur.execute("UPDATE playerTable SET rep=%s WHERE discordID=%s;" % (rep, target.id))
+        database.conn.commit()
+        await ctx.send("%s has been given %s rep." % (target.mention, drep))
     else:
         await ctx.send(NOT_OWNER_MESSAGE)
