617ea33d7e735407f1df1e3a026ad460efe01567
Nick Greene <nickjg01@gmail.com>
2018-10-15 09:44:37 -0500

    try again

--- owner_commands.py
+++ owner_commands.py
@@ -4,16 +4,16 @@ from bot import bot
 
 import database
 
-async def owner_command(ctx, func):
+async def owner_command(func, ctx):
     async def wrapper(ctx, *args, **kwargs):
         if ctx.message.author.id == discord.AppInfo.owner.id:
-            func(ctx, *args, **kwargs)
+            await func(ctx, *args, **kwargs)
         else:
             await ctx.send("This command is only for use by the owner.")
     return wrapper
 
 @bot.command()
-@owner_command()
+@owner_command
 async def giveelo(ctx, target: discord.User, delo):
     elo = database.player_elo(target.id)
     elo += delo
