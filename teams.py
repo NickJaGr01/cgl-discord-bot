9c8bd15103be972495cc07659dfba0ba867ebb8c
Nick Greene <nickjg01@gmail.com>
2018-10-22 13:45:26 -0500

    debug

--- teams.py
+++ teams.py
@@ -5,4 +5,4 @@ async def process_invite(reaction, user):
     if reaction.message.author.id == bot.appinfo.id:
         if reaction.message.content.startswith("You have been invited to join"):
             team = reaction.message.content[30:-40]
-            print("team = %s" % team)
+            print(team)
