83c2ba73ac3eda0ebe5e31d7e398679a40ab7919
Nick Greene <nickjg01@gmail.com>
2018-10-22 12:46:31 -0500

    syntax error

--- teams.py
+++ teams.py
@@ -1,7 +1,7 @@
 from bot import bot
 
 async def process_invite(reaction, user):
-    if reaction.message.author.id = bot.appinfo.id:
+    if reaction.message.author.id == bot.appinfo.id:
         if reaction.message.content.startswith("You have been invited to join"):
             team = reaction.message.content[30:-40]
             print("team = %s" % team)
