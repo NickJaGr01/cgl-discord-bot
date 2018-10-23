480340f29f8f8ae52d5f22fb969434effae9bffe
Nick Greene <nickjg01@gmail.com>
2018-10-23 14:03:57 -0500

    big dum

--- teams_cog.py
+++ teams_cog.py
@@ -116,7 +116,7 @@ class Teams:
         if database.user_registered(ctx.author.id):
             #check that the user is on a team
             database.cur.execute("SELECT teamname FROM teamTable WHERE captainID=%s;" % ctx.author.id)
-            team = database.cur.fetchone()[0]
+            team = database.cur.fetchone()
             if team == None:
                 await ctx.send("You are not the captain of a team.")
                 return
