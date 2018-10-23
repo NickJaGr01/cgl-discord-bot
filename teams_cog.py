3f38c09cf60c71af5da7a5d33def38b3fd1f253c
Nick Greene <nickjg01@gmail.com>
2018-10-23 13:52:29 -0500

    test

--- teams_cog.py
+++ teams_cog.py
@@ -33,7 +33,8 @@ class Teams:
                 return
             #create the team
             teamrole = await bot.guild.create_role(name=teamname, colour=discord.Colour.orange(), hoist=True)
-            await teamrole.edit(position=bot.guild.get_role(bot.FREE_AGENT_ROLE).position+1)
+            #await teamrole.edit(position=bot.guild.get_role(bot.FREE_AGENT_ROLE).position+1)
+            await teamrole.edit(position=1)
             await bot.guild.get_member(ctx.author.id).add_roles(teamrole)
             database.cur.execute("INSERT INTO teamTable (teamname, stats, captainID, teamRoleID) VALUES ('%s', '%s', %s, %s);" % (teamname, json.dumps(TEAM_STATS_DICT), ctx.author.id, teamrole.id))
             database.cur.execute("UPDATE playerTable SET team='%s' WHERE discordID=%s;" % (teamname, ctx.author.id))
