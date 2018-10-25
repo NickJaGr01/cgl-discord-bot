90b83cd5f5179a6f584b2eeb227a872502cdd3f7
Nick Greene <nickjg01@gmail.com>
2018-10-25 12:14:49 -0500

    fixed converter

--- cgl_converters.py
+++ cgl_converters.py
@@ -5,12 +5,11 @@ from bot import bot
 
 class CGLUser(commands.UserConverter):
     async def convert(cls, ctx, argument):
-        user = None
         try:
             user = await super().convert(ctx, argument)
+            return user
         except:
             database.cur.execute("SELECT discordID FROM playerTable WHERE username='%s';" % argument)
             discordid = database.cur.fetchone()
             if discordid != None:
-                user = bot.get_user(discordid[0])
-        return user
+                return = bot.get_user(discordid[0])
