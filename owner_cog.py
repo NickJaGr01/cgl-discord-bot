9691f3710df5e996916aa5c45d6981343e744c9c
Nick Greene <nickjg01@gmail.com>
2018-11-06 12:13:17 -0600

    added !awardplayer

--- owner_cog.py
+++ owner_cog.py
@@ -21,12 +21,22 @@ class Owner:
 
     @commands.command(pass_context=True)
     async def awardteam(self, ctx, team: CGLTeam, *, award):
-        """give a team an award"""
+        """give a team and its players an award"""
         if ctx.message.author.id == bot.appinfo.owner.id:
             database.cur.execute("UPDATE teamTable SET awards=array_append(awards, '%s') WHERE teamname='%s';" % (award, team.teamname))
             database.cur.execute("UPDATE playerTable SET awards=array_append(awards, '%s') WHERE team='%s';" % (award, team.teamname))
             database.conn.commit()
             await ctx.send("%s and all its players have been awarded %s." % (team.teamname, award))
+        else:
+            await ctx.send(NOT_OWNER_MESSAGE)
+
+    @commands.command(pass_context=True)
+    async def awardplayer(self, ctx, player: CGLUser, *, award):
+        """give a player an award"""
+        if ctx.message.author.id == bot.appinfo.owner.id:
+            database.cur.execute("UPDATE playerTable SET awards=array_append(awards, '%s') WHERE discordID=%s;" % (award, player.id))
+            database.conn.commit()
+            await ctx.send("%s has been awarded %s." % (database.username(player.id), award))
         else:
             await ctx.send(NOT_OWNER_MESSAGE)
 
