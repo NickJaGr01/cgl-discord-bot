3c9105e972b72fb87b668cc0785a4f3dc0613991
Nick Greene <nickjg01@gmail.com>
2018-11-13 13:15:05 -0600

    added !adjustelo

--- owner_cog.py
+++ owner_cog.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord
 from bot import bot
-from utils import *
+import utils
 import database
 from cgl_converters import *
 
@@ -18,7 +18,7 @@ class Owner:
             database.conn.commit()
             targetusername = database.username(target.id)
             await ctx.send("%s has been given %s elo." % (targetusername, delo))
-            await log("OWNER: %s gave %s rep to %s." % (database.username(ctx.author.id), drep, targetusername))
+            await utils.log("OWNER: %s gave %s rep to %s." % (database.username(ctx.author.id), drep, targetusername))
         else:
             await ctx.send(NOT_OWNER_MESSAGE)
 
@@ -30,7 +30,7 @@ class Owner:
             database.cur.execute("UPDATE playerTable SET awards=array_append(awards, '%s') WHERE team='%s';" % (award, team.teamname))
             database.conn.commit()
             await ctx.send("%s and all its players have been awarded %s." % (team.teamname, award))
-            await log("OWNER: %s gave the award %s to %s." % (database.username(ctx.author.id), award, team.teamname))
+            await utils.log("OWNER: %s gave the award %s to %s." % (database.username(ctx.author.id), award, team.teamname))
         else:
             await ctx.send(NOT_OWNER_MESSAGE)
 
@@ -42,7 +42,7 @@ class Owner:
             database.conn.commit()
             targetusername = database.username(player.id)
             await ctx.send("%s has been awarded %s." % (targetusername, award))
-            await log("OWNER: %s gave the award %s to %s." % (database.username(ctx.author.id), award, targetusername))
+            await utils.log("OWNER: %s gave the award %s to %s." % (database.username(ctx.author.id), award, targetusername))
         else:
             await ctx.send(NOT_OWNER_MESSAGE)
 
@@ -52,7 +52,7 @@ class Owner:
         if ctx.message.author.id == bot.appinfo.owner.id:
             database.cur.execute("UPDATE settings SET string='%s' WHERE key='sponsor_ad';" % (award, player.id))
             await ctx.send("The sponsor advertisement has been updated.")
-            await log("OWNER: the sponsor advertisement has been updated:\n%s" % ad)
+            await utils.log("OWNER: the sponsor advertisement has been updated:\n%s" % ad)
             announcements = bot.guild.get_channel(bot.ANNOUNCEMENTS_CHANNEL)
             database.cur.execute("SELECT int FROM settings WHERE key='last_ad_id';")
             lastad = database.cur.fetchone()[0]
@@ -62,6 +62,39 @@ class Owner:
                 newmsg = await announcements.send(ad)
                 database.cur.execute("UPDATE settings SET int=%s WHERE key='last_sponsor_id';" % newmsg.id)
             database.conn.commit()
+        else:
+            await ctx.send(NOT_OWNER_MESSAGE)
+
+    @commands.command(pass_context=True)
+    async def adjustelo(self, ctx, team1size: int, team1score: int, team2score: int, *players: CGLUser):
+        """adjust elo after a match"""
+        k_factor = 128
+        if ctx.message.author.id == bot.appinfo.owner.id:
+            rounds = team1score+team2score
+            team1avg = 0
+            team2avg = 0
+            for p in players[:team1size]:
+                team1avg += database.player_elo(p.id)
+            for p in players[team1size:]:
+                team2avg += database.player_elo(p.id)
+            team1avg /= team1size
+            team2avg /= team2size
+            team1exp = 1/(1+pow(10, (team2avg-team1avg)/400))
+            team2exp = 1/(1+pow(10, (team1avg-team2avg)/400))
+            delo1 = k_factor * ((team1score/rounds) - team1exp)
+            delo2 = k_factor * ((team2score/rounds) - team2exp)
+            for p in players[:team1size]:
+                elo = database.player_elo(p.id)
+                elo += delo1
+                database.cur.execute("UPDATE playerTable SET elo=%s WHERE discordID=%s;" % (elo, p.id))
+                await utils.log("%s has been given %s elo." % (database.username(p.id), delo1))
+            for p in players[team1size:]:
+                elo = database.player_elo(p.id)
+                elo += delo2
+                database.cur.execute("UPDATE playerTable SET elo=%s WHERE discordID=%s;" % (elo, p.id))
+                await utils.log("%s has been given %s elo." % (database.username(p.id), delo2))
+            database.conn.commit()
+            await ctx.send("Elo has been updated for those players.")
         else:
             await ctx.send(NOT_OWNER_MESSAGE)
 
