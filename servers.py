bb3ee8676bc4eebf2ef4eb0a241ea90938cb5edb
Nick Greene <nickjg01@gmail.com>
2018-11-14 17:49:14 -0600

    Update servers.py

--- servers.py
+++ servers.py
@@ -25,7 +25,7 @@ def create_server(name, location, map):
         'Content-Type': 'application/x-www-form-urlencoded',
         'Accept': 'application/json'
     }
-    r = requests.post('https://dathost.net/api/0.1/game-servers', auth=(login_email, login_pass), data=json.dumps(p))
+    r = requests.post('https://dathost.net/api/0.1/game-servers', auth=(login_email, login_pass), data=p)
     print(r.status_code)
     return r.json()
 
