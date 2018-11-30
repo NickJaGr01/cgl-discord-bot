c3ff7de692600ac50578f04040572b0c38f80a19
Nick Greene <nickjg01@gmail.com>
2018-11-30 09:40:24 -0600

    Update owner_cog.py

--- owner_cog.py
+++ owner_cog.py
@@ -114,10 +114,10 @@ class Owner:
     @commands.is_owner()
     async def dostuff(self, ctx):
         """do stuff"""
-        e = discord.Embed(title="Hello!", inline=True)
+        e = discord.Embed(title="Hello!", inline=False)
         e.add_field(name="key 1", value="value 1", inline=True)
         e.add_field(name="key 2", value="value 2", inline=True)
-        e.add_field(name="key 3", value="value 3", inline=True)
+        e.add_field(name="key 3", value="value 3", inline=False)
         e.add_field(name="key 4", value="value 4", inline=True)
         await ctx.send(embed=e)
 
