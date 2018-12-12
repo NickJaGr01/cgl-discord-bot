4b6cfb49ff8a84a8c19ad3f4a5a0ab6d351151b9
Nick Greene <nickjg01@gmail.com>
2018-12-12 08:21:45 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from datetime import datetime
 import database
 
 bot = commands.Bot(command_prefix='!')
+bot.remove_command("help")
 
 import teams
 
@@ -58,7 +59,61 @@ async def on_ready() :
     bot.STANDIN_CHANNEL = int(os.environ['STANDIN_CHANNEL'])
     bot.TEAMS_BOTTOM_END_ROLE = int(os.environ['TEAMS_BOTTOM_END_ROLE'])
     bot.TEAMS_TOP_END_ROLE = int(os.environ['TEAMS_TOP_END_ROLE'])
+    bot.MAX_TEAM_SIZE = 12
 
     #bot.task = asyncio.create_task(background_thread())
+
+@bot.command()
+async def help(ctx, *commands : str):
+    """Shows this message."""
+    destination = ctx.message.author if bot.pm_help else ctx.message.channel
+
+    def repl(obj):
+        return _mentions_transforms.get(obj.group(0), '')
+
+    # help by itself just lists our own commands.
+    if len(commands) == 0:
+        pages = bot.formatter.format_help_for(ctx, bot)
+    elif len(commands) == 1:
+        # try to see if it is a cog name
+        name = _mention_pattern.sub(repl, commands[0])
+        command = None
+        if name in bot.cogs:
+            command = bot.cogs[name]
+        else:
+            command = bot.commands.get(name)
+            if command is None:
+                yield from bot.send_message(destination, bot.command_not_found.format(name))
+                return
+
+        pages = bot.formatter.format_help_for(ctx, command)
+    else:
+        name = _mention_pattern.sub(repl, commands[0])
+        command = bot.commands.get(name)
+        if command is None:
+            yield from bot.send_message(destination, bot.command_not_found.format(name))
+            return
+
+        for key in commands[1:]:
+            try:
+                key = _mention_pattern.sub(repl, key)
+                command = command.commands.get(key)
+                if command is None:
+                    yield from bot.send_message(destination, bot.command_not_found.format(key))
+                    return
+            except AttributeError:
+                yield from bot.send_message(destination, bot.command_has_no_subcommands.format(command, key))
+                return
+
+        pages = bot.formatter.format_help_for(ctx, command)
+
+    if bot.pm_help is None:
+        characters = sum(map(lambda l: len(l), pages))
+        # modify destination based on length of pages.
+        if characters > 1000:
+            destination = ctx.message.author
+
+    for page in pages:
+        yield from bot.send_message(destination, page)
 
 import events
