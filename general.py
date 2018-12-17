536a1ca6ed4e2d120e9c0ca4945779d796089855
Nick Greene <nickjg01@gmail.com>
2018-12-17 12:02:08 -0600

    Update general.py

--- general.py
+++ general.py
@@ -4,6 +4,7 @@ async def process_get_roles(reaction, user):
     if reaction.message.content.startswith("Please select your roles"):
         if reaction.emoji == "✅":
             member = bot.guild.get_member(user.id)
+            reactions = reaction.message.reactions
             for r in reactions:
                 if r.emoji in bot.LIST_EMOJIS:
                     index = bot.LIST_EMOJIS.index(r.emoji)
@@ -14,4 +15,5 @@ async def process_get_roles(reaction, user):
                     elif r.count == 1:
                         if role in member.roles:
                             await member.remove_roles(role)
+            await reaction.message.delete()
             await user.send("Your roles have been updated")
