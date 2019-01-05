9f7a2a566cea759cb9344b6fcfeacb3a1164d31d
Nick Greene <nickjg01@gmail.com>
2019-01-05 12:15:15 -0600

    removed redis

--- http_server.py
+++ http_server.py
@@ -1,16 +1,11 @@
 import tornado.ioloop
 import tornado.web
 import os
-import redis
-
-REDIS_URL = os.environ['REDIS_URL']
-r = redis.Redis.from_url(REDIS_URL)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello World!")
         print("ping received")
-        r.publish('requests', "Hello World!")
 
 def make_app():
     return tornado.web.Application([(r"/", MainHandler),])
--- matches.py
+++ matches.py
@@ -1,2 +1,13 @@
+class Match:
+    def __init__(self, team1, team2, map):
+        self.team1 = team1
+        self.team2 = team2
+        self.map = map
+
 async def check_matches():
+    pass
+
+
+
+def create_match():
     pass
