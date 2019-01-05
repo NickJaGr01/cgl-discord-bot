fa6a823798dbcce7a17cc8ef3d909de09cb77aea
Nick Greene <nickjg01@gmail.com>
2019-01-04 20:21:48 -0600

    i have super thicc stupid

--- http_server.py
+++ http_server.py
@@ -9,6 +9,6 @@ def make_app():
     return tornado.web.Application([(r"/", MainHandler),])
 
 def start_listening():
-    app = make_app
+    app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
