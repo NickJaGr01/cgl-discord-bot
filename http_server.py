c992b89f5e37b06f77f5bd80753ea3291e3b31c2
Nicholas Greene <nickjg01@gmail.com>
2019-01-06 17:19:35 -0600

    Update http_server.py

--- http_server.py
+++ http_server.py
@@ -47,6 +47,7 @@ class ConfigHandler(tornado.web.RequestHandler):
         team1_name, team1_players, team2_name, team2_players, map = database.cur.fetchone()
         config = generate_config(team1_name, team1_players, team2_name, team2_players, map)
         #self.content_type='application/json'
+        print("sending config")
         self.set_header("Content-Type", "application/json")
         self.write(config)
 
