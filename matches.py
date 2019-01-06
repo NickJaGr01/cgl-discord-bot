3a4767c8f2f6d9e381900a205cc6a8378a462293
Nicholas Greene <nickjg01@gmail.com>
2019-01-06 16:14:12 -0600

    Update matches.py

--- matches.py
+++ matches.py
@@ -6,7 +6,7 @@ import uuid
 class Match:
     def __init__(self, team1_name, team1_players, team2_name, team2_players, map, location):
         self.team1_name = team1_name
-        self.team1_name = team1_players
+        self.team1_players = team1_players
         self.team2_name = team2_name
         self.team2_players = team2_players
         self.map = map
